df3b309ab62f7b5b1790959d71f6747b68ac460e
aedart <aedart@gmail.com>
2022-02-13 13:29:00 +0000

    (Re)define matrix for all packages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Translations / languages lines for Athenaeum validation package.
+    |
+    */
+
+    'alpha_dash_dot' => 'The :attribute may only contain letters, numbers, dashes, underscores and dots.',
+    'sem_version' => ':attribute must be stated as a valid Semantic Versioning number'
+];
